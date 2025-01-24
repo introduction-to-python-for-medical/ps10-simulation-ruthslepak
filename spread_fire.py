7849424624a33cfbb0e993fac915df6255006b47
ruthslepak <rootush@gmail.com>
2025-01-24 16:55:15 +0200

    Update spread_fire.py

--- spread_fire.py
+++ spread_fire.py
@@ -25,18 +25,29 @@ def initialize_forest(grid_size=30, p_tree=0.6):
 
     return grid
 
-# define the rules for spreading the fire
 def spread_fire(grid):
     """Update the forest grid based on fire spreading rules."""
+    grid_size = len(grid)
     update_grid = copy.deepcopy(grid)
-    for i in range(grid_size-1):
-        for j in range(grid_size-1):
-            if grid[i][j] == 1:
-                neighbors = [grid[i-1][j],grid[i+1][j],grid[i][j-1],grid[i][j+1]]
+
+    for i in range(grid_size):
+        for j in range(grid_size):
+            if grid[i][j] == 1:  # עץ
+                neighbors = []
+                if i > 0:  # עליון
+                    neighbors.append(grid[i-1][j])
+                if i < grid_size - 1:  # תחתון
+                    neighbors.append(grid[i+1][j])
+                if j > 0:  # שמאלי
+                    neighbors.append(grid[i][j-1])
+                if j < grid_size - 1:  # ימני
+                    neighbors.append(grid[i][j+1])
+                # בדיקה אם יש שכנים בוערים
                 if 2 in neighbors:
                     update_grid[i][j] = 2
 
     return update_grid
+
  
 
 # Set up the grid
